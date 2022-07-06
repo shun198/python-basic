3395f67e7c6a1137064f45c2bdc4d2ddffdbda81
shun198 <shunhiroseluvmri@gmail.com>
2022-07-07 07:42:43 +0900

    :+1:add:list and set python files

---
+++ list_basic.py
@@ -0,0 +1,7 @@
+fruits = ['apple', 'grape', 'orange']
+print(fruits)
+shop_list = ['cherry', 8, 3, False, fruits]
+print(shop_list)
+print(shop_list[0])
+print(shop_list[-4])
+print(shop_list[-1][2])
---
+++ set_basic.py
@@ -0,0 +1,6 @@
+meet = {'pork', 'beef', 'chicken'}
+print(meet)
+
+tea = {'earl grey', 'darjeeling', 'earl grey', 'chai'}
+print(tea)
+# print(tea[0])
