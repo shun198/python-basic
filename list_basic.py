9217d3f42b34fe8a7ab36c5437d33bb6015c2879
s-hirose <shunhiroseluvmri@gmail.com>
2022-07-07 16:04:12 +0900

    :+1: add list_basic.py

--- list_basic.py
+++ list_basic.py
@@ -1,3 +1,4 @@
+from collections import deque
 fruits = ['apple', 'grape', 'orange']
 print(fruits)
 shop_list = ['cherry', 8, 3, False, fruits]
@@ -5,3 +6,22 @@ print(shop_list)
 print(shop_list[0])
 print(shop_list[-4])
 print(shop_list[-1][2])
+
+stack = [1, 2, 3]
+stack.append(4)
+stack.append(5)
+print(stack)
+stack.pop()
+print(stack)
+stack.pop()
+print(stack)
+
+queue = deque(["Eric", "John", "Michael"])
+queue.append("Terry")           # Terry arrives
+queue.append("Graham")          # Graham arrives
+print(queue)
+queue.popleft()                 # The first to arrive now leaves
+print(queue)
+queue.popleft()                 # The second to arrive now leaves
+print(queue)
+# Remaining queue in order of arrival
---
+++ numeric_basic.py
@@ -0,0 +1,13 @@
+# int
+print(type(-100))
+# float
+print(type(0.1))
+print(0.1+0.1+0.1)
+
+# 四則演算
+print(2 + 3)
+print(7 - 3)
+print(2 * 4)
+print(8 / 2)
+print(14 // 3)
+print(14 % 3)
