b29cf06158f4d1452a79488bf4f0ee26f84e641b
shun198 <shunhiroseluvmri@gmail.com>
2022-07-09 14:36:11 +0900

    add:for.py

---
+++ for.py
@@ -0,0 +1,15 @@
+# for
+fruits = ['apple', 'banana', 'orange', 'grape']
+for fruit in fruits:
+    print(f"I like {fruit}")
+
+
+# range(start, stop, step)
+for i in range(2):
+    print(i)
+
+for i in range(2, 4):
+    print(i)
+
+for i in range(4, 9, 2):
+    print(i)
