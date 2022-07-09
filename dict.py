3399b0ed62a87d3a22fbd10b1fa7565cf8dcb5b3
shun198 <shunhiroseluvmri@gmail.com>
2022-07-09 17:25:46 +0900

    add dict.py

--- dict.py
+++ dict.py
@@ -1,2 +1,18 @@
 fruits = {1: "apple", 2: "banana", 3: "orange"}
 print(fruits[1])
+
+for fruit in fruits.keys():
+    print(fruit)
+
+for fruit in fruits.values():
+    print(fruit)
+
+for key, value in fruits.items():
+    print(key)
+    print(value)
+
+print(fruits.get(4, "fruits not found"))
+
+fruits_2 = {4: "grape", 5: "cherry", 6: "mango"}
+fruits.update(fruits_2)
+print(fruits)
