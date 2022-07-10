6acc707215cb08434701e7fd0117efd614e0d68d
shun198 <shunhiroseluvmri@gmail.com>
2022-07-10 17:16:27 +0900

    add docstring

---
+++ function/docstring.py
@@ -0,0 +1,11 @@
+def multiply(num1, num2):
+    """
+    Multiply two numbers
+    :param num1:first number to multiply
+    :param num2:second number to multiply
+    :return:num1 * num2
+    """
+    return num1 * num2
+
+
+multiply(1, 2)
