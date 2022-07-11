3f1fb3ac43e33e735309129afeb54a1158e88b00
shun198 <shunhiroseluvmri@gmail.com>
2022-07-11 19:20:18 +0900

    :+1: #14 add modules

--- module/mymodule.py
+++ module/mymodule.py
@@ -3,3 +3,7 @@ global_variable = "This is a global variable"
 
 def myfunc():
     print("Hello from a function in mymodule.py")
+
+
+print("This is mymodule.py")
+print(f"mymodule.__name__: {__name__}")
