99c2cc6fb68fc84e3415a613058473f679f15667
shun198 <shunhiroseluvmri@gmail.com>
2022-07-14 05:50:52 +0900

    :+1: #18 add except and finally

--- exception/try_except.py
+++ exception/try_except.py
@@ -4,13 +4,14 @@ def split_bill(price):
         each = price / int(num)
     except ZeroDivisionError as e:
         print("0で割ることができません。正しい値を入力してください")
-        each = price
         print(e)
     except ValueError as e:
         print("文字列ではなく、数字を入力してください")
-        each = 0
         print(e)
-    print(f"1人あたり{each}円")
+    else:
+        print(f"1人あたり{each}円")
+    finally:
+        print("処理が終了しました")
 
 
 if __name__ == "__main__":
