5ff9448126b194803140c7f9cc596e6da75d35b9
shun198 <shunhiroseluvmri@gmail.com>
2022-07-20 06:47:34 +0900

    add:mymodule and myscript

--- function/encapsulation.py
+++ function/encapsulation.py
@@ -1,7 +1,7 @@
 # カプセル化(encapsulation) 外部から変更できないようにする
 def casino_entrance(age, min_age=21):
     if age < min_age:
-        print(f'You are {age} years old.Too young to enter')
+        print(f"You are {age} years old.Too young to enter")
         return
 
     def inner_casino_entrance():
--- module/myscript.py
+++ module/myscript.py
@@ -1,6 +1,7 @@
 import sys
 import mymodule
 import numpy as np
+
 mymodule.myfunc()
 print(mymodule.global_variable)
 print(np.__version__)
